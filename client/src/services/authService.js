be59c83faa53061bc9c652a5e0ab6a9891a9a86a
Abhinav Lakhara <71587621+Abhil01@users.noreply.github.com>
2025-05-13 01:58:53 +0530

    Delete client directory

--- client/src/services/authService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = 'http://localhost:5000/api/auth'; 
-
-export const login = (data) => axios.post(`${API}/login`, data);
-export const signup = (data) => axios.post(`${API}/signup`, data);
